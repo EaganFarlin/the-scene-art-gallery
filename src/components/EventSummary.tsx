bda329383646b5ec05d0314be6ff33aece741b5c
6cpsmidnight <cyberelectro101@gmail.com>
2024-01-03 17:37:45 -0500

    Mobile responsive modifications

--- src/components/EventSummary.tsx
+++ src/components/EventSummary.tsx
@@ -13,12 +13,13 @@ export default function EventSummary({
 }) {
   return (
     <div className="mt-12">
-      <p className="text-2xl">{title}</p>
-      <p className="text-xl">
-        <span className="mr-4">{date}</span>|
-        <span className="ml-4">{time}</span>
-      </p>
-      <p className="text-xl text-gray-600">{desc}</p>
+      <p className="text-3xl">{title}</p>
+      <div className="lg:flex lg:flex-wrap text-2xl">
+        <p className="mr-4">{date}</p>
+        {window.innerWidth > 1024 && <p className="mr-4">|</p>}
+        <p>{time}</p>
+      </div>
+      <p className="text-2xl text-gray-600">{desc}</p>
     </div>
   );
 }
